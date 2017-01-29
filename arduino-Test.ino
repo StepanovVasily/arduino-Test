185bf3c4cfd0ef9501f963b560b627e068674fb5
StepanovVasily <01vasil@mail.ru>
2017-01-29 23:50:48 +0300

    29.01.17

--- arduino-Test.ino
+++ arduino-Test.ino
@@ -19,6 +19,7 @@ delay(300);
   pinMode (23,OUTPUT);
   pinMode (25,OUTPUT);
   pinMode (27,OUTPUT);
+  pinMode(A2,INPUT);//датчик углекислоты 
   //pinMode(A0, INPUT);
   //pinMode(A1, INPUT);
   //pinMode(A9, INPUT);
@@ -32,7 +33,7 @@ void loop()
 ////////////вывод времени
 
 
-lcd.setCursor(8, 1);
+lcd.setCursor(6, 1);
 lcd.print(time.gettime("H:i")); // выводим время
 
 //lcd.print(time.gettime("H:i:s")); // выводим время
@@ -80,7 +81,6 @@ lcd.setCursor(0, 0);
  int light=analogRead(A1);
  // if (isnan(t) || isnan(h)) { 
     
- lcd.print("L ");
   light=(1023-light);
 
    lcd.print(light); 
@@ -93,24 +93,48 @@ int MINUTE=time.minutes;
 
 
 ///реле 1
-if((HOUR>1)&&(HOUR<16)&&(light<940))digitalWrite(23,1);
-if((HOUR>1)&&(HOUR<16)&&(light>950))digitalWrite(23,0);
-if((HOUR<1)||(HOUR>16))digitalWrite(23,0);
+if((HOUR>0)&&(HOUR<13)&&(light<940)){digitalWrite(23,1);lcd.setCursor(15,1);lcd.print('I');}
+if((HOUR>0)&&(HOUR<13)&&(light>950)){digitalWrite(23,0);lcd.setCursor(15,1);lcd.print(' ');}
+if((HOUR<0)||(HOUR>13)){digitalWrite(23,0);lcd.setCursor(15,1);lcd.print(' ');}
 
 //// реле 2
-if(t<20)digitalWrite(25,1);
-if(t>26)digitalWrite(25,0);
+if(t<8){digitalWrite(25,1);lcd.setCursor(14,1);lcd.print('I');}
+if(t>10){digitalWrite(25,0);lcd.setCursor(14,1);lcd.print(' ');}
 
 
 ////реле 3
 
-if((MINUTE>0)&&(MINUTE<30))digitalWrite(27,1);else digitalWrite(27,0);
+if((MINUTE>0)&&(MINUTE<30)){digitalWrite(27,1);lcd.setCursor(13,1);lcd.print('I');}
+else {digitalWrite(27,0);lcd.setCursor(13,1);lcd.print(' ');}
 
 
+
+//датчик углекислоты
+int f=0;
+for(int i=0;i!=50;i++){
+  delay(10);
+f=f+analogRead(A2);}
+f=f/50;
+
+int f1=0;
+for(int i=0;i!=50;i++){
+  delay(10);
+f1=f1+analogRead(A2);}
+f1=f1/50;
+
+
+f=(f+f1)/2;
+f=341-f;
+float ff=f*28.3+350;
+
+
+
+//
 
 
  
-  delay(10000);
+
+  delay(9000);
   lcd.clear();
    
     
@@ -135,97 +159,3 @@ int light=analogRead(A1);
    lcd.print(light); 
 
 }
-
-
-
-
-/*
-  int pin =5;
-  #include "ardumidi.h"
-
-  int ledPin = 13;
-  int note_on = 0;
-  int st=0;
-  void setup()
-  {
-  Serial.begin(256000);
-  pinMode(ledPin, OUTPUT);
-  pinMode(pin, INPUT);
-
-  }
-
-  void loop()
-  {
-
-  digitalWrite(5,1);
-
-  if (!digitalRead(5))
-  {
-  // play a Cminor chord on channel 0, at maximum volume (127)
-  if(st==0){
-  st=1;
-    midi_note_on(0, MIDI_C, 127);
-    midi_note_on(0, MIDI_E + MIDI_FLAT + MIDI_OCTAVE, 127);
-    midi_note_on(0, MIDI_G + MIDI_OCTAVE, 127);
-
-  // The MIDI_* macros were created to make your life easier, but you
-  // don't have to use them. You may enter numbers instead if you know
-  // what you're doing.
-
-    note_on = 1;
-    digitalWrite(ledPin, HIGH);
-  }
-  }
-
-  else
-  {
-    if(st==1){
-    st=0;
-  // stop the Cminor chord
-    midi_note_off(0, MIDI_C, 127);
-    midi_note_off(0, MIDI_E + MIDI_FLAT + MIDI_OCTAVE, 127);
-    midi_note_off(0, MIDI_G + MIDI_OCTAVE, 127);
-
-    note_on = 0;
-    digitalWrite(ledPin, LOW);
-    delay(100);
-    }
-  }
-
-
-  }*/
-
-// Available commands:
-//
-// Start/stop playing a certain note:
-//   midi_note_on(byte channel, byte key, byte velocity);
-//   midi_note_off(byte channel, byte key, byte velocity);
-//
-// Change pressure of specific keys:
-//   midi_key_pressure(byte channel, byte key, byte value);
-//
-// Change controller value (used for knobs, etc):
-//   midi_controller_change(byte channel, byte controller, byte value);
-//
-// Change "program" (change the instrument):
-//   midi_program_change(byte channel, byte program);
-//
-// Change key pressure of entire channels:
-//   midi_channel_pressure(byte channel, byte value);
-//
-// Change pitch-bend wheel:
-//   midi_pitch_bend(byte channel, int value);
-//
-// Send a comment:
-//   midi_comment(char* str);
-//
-// Send a series of bytes (to be interpreted by another program):
-//   midi_printbytes(char* bytes, int len);
-//
-// Parameters:
-//   channel             an integer from 0 to 15
-//   pitch-bend value    an integer from 0 to 16383
-//   all other values    an integer from 0 to 127
-//
-
-
